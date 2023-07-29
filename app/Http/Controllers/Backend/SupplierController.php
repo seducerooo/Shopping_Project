b5c96737bea006c5e44cba989d9260560ec50bbe
seducerooo <94935126+seducerooo@users.noreply.github.com>
2023-07-29 21:10:39 +0330

    in views/backend directory supplier dirextory created &&in views/backend/supplier directory add_supplier blade file and all_supplier blade file and edit_supplier blade file created && in SupplierController AllSupplier method and AddSupplier method and StoreSupplier method and EditSupplier method and UpdateSupplier method and DestroySupplier method created

--- app/Http/Controllers/Backend/SupplierController.php
+++ app/Http/Controllers/Backend/SupplierController.php
@@ -3,9 +3,140 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Supplier;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Carbon;
 
 class SupplierController extends Controller
 {
     //
+    public function AllSupplier(){
+        $allSupplier = Supplier::query()->latest()->get();
+        return view('backend.supplier.all_supplier',compact('allSupplier'));
+    }
+    public function AddSupplier(){
+        return view('backend.supplier.add_supplier',);
+    }
+    public function StoreSupplier(Request $request){
+
+        $validateData = $request->validate([
+
+            'name' => 'required|max:200',
+            'email' => 'required|max:200|unique:customers',
+            'phone' => 'required|max:200',
+            'address' => 'required|max:400',
+            'shopname' => 'required|max:200',
+            'type' => 'required',
+            'account_holder' => 'required|max:200',
+            'account_number' => 'required',
+            'bank_name' => 'required',
+            'bank_branch' => 'required',
+            'image' => 'required',
+        ],
+            [
+                'name.required' => 'Please Enter Name Field',
+                'email.required' => 'Please Enter Email Field',
+                'phone.required' => 'Please Enter Phone Field',
+                'address.required' => 'Please Enter Address Field',
+                'type.required' => 'Please Enter Type Field',
+                'shopname.required' => 'Please Enter Shop Name Field',
+                'account_holder.required' => 'Please Enter account_holder Field',
+                'account_number.required' => 'Please Enter account_number Field',
+                'bank_name.required' => 'Please Enter Bank Name Field',
+                'bank_branch.required' => 'Please Enter Bank Branch Field',
+                'image.required' => 'Please Enter Image Field'
+            ]
+        );
+
+        $image = $request->file('image');
+        $name_gen = hexdec(uniqid()). '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('upload/supplier_images/'.$name_gen);
+        $save_url = 'upload/supplier_images/'.$name_gen;
+        $supplier = new Supplier();
+        $supplier->name = $request['name'];
+        $supplier->email = $request['email'];
+        $supplier->phone = $request['phone'];
+        $supplier->address = $request['address'];
+        $supplier->shopname = $request['shopname'];
+        $supplier->account_holder = $request['account_holder'];
+        $supplier->account_number = $request['account_number'];
+        $supplier->bank_name = $request['bank_name'];
+        $supplier->bank_branch = $request['bank_branch'];
+        $supplier->image = $save_url;
+        $supplier->city = $request['city'];
+        $supplier->created_at = Carbon::now();
+        $supplier->save();
+        $notification = array([
+            'message' => 'Supplier Recorde Stored Successfully',
+            'alert-type' => 'success'
+        ]);
+        return to_route('all.supplier')->with($notification[0]);
+    }
+
+    public function EditSupplier(Request $request,string $id){
+        $supplier = Supplier::query()->find($id);
+        return view('backend.supplier.edit_supplier',compact('supplier'));
+    }
+
+
+    public function UpdateSupplier(Request $request,string $id){
+        $supplier_id = $id;
+        if ($request->file('image')){
+            $image = $request->file('image');
+            $name_gen = hexdec(uniqid()). '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(300,300)->save('upload/supplier_images/'.$name_gen);
+            $save_url = 'upload/supplier_images/'.$name_gen;
+
+            $supplier_update = Supplier::query()->find($supplier_id);
+            $supplier_update->name = $request['name'];
+            $supplier_update->email = $request['email'];
+            $supplier_update->phone = $request['phone'];
+            $supplier_update->address = $request['address'];
+            $supplier_update->shopname = $request['shopname'];
+            $supplier_update->account_holder = $request['account_holder'];
+            $supplier_update->account_number = $request['account_number'];
+            $supplier_update->bank_name = $request['bank_name'];
+            $supplier_update->bank_branch = $request['bank_branch'];
+            @unlink(public_path($supplier_update->image));
+            $supplier_update->image = $save_url;
+            $supplier_update->save();
+            $notification = array([
+                'message' => 'Supplier Recorde With Image Updated Successfully',
+                'alert-type' => 'success'
+            ]);
+            return to_route('all.supplier')->with($notification[0]);
+        }
+        $supplier_update = Supplier::query()->find($supplier_id);
+        $supplier_update->name = $request['name'];
+        $supplier_update->email = $request['email'];
+        $supplier_update->phone = $request['phone'];
+        $supplier_update->address = $request['address'];
+        $supplier_update->shopname = $request['shopname'];
+        $supplier_update->account_holder = $request['account_holder'];
+        $supplier_update->account_number = $request['account_number'];
+        $supplier_update->bank_name = $request['bank_name'];
+        $supplier_update->bank_branch = $request['bank_branch'];
+        $supplier_update->city = $request['city'];
+        $supplier_update->save();
+        $notification = array([
+            'message' => 'Supplier Recorde Without Image Updated Successfully',
+            'alert-type' => 'success'
+        ]);
+        return to_route('all.supplier')->with($notification[0]);
+
+
+    }
+
+
+    public function DestroySupplier(string $id){
+        $supplier_destroy = Supplier::query()->find($id);
+        @unlink(public_path($supplier_destroy->image));
+        $supplier_destroy->delete();
+        $notification = array([
+            'message' => 'Supplier Recorde Deleted Successfully',
+            'alert-type' => 'success'
+        ]);
+        return to_route('all.supplier')->with($notification[0]);
+    }
 }
---
+++ resources/views/backend/supplier/all_supplier.blade.php
@@ -0,0 +1,82 @@
+@extends('admin_dashboard')
+@section('admin')
+
+
+    <div class="content">
+
+        <!-- Start Content-->
+        <div class="container-fluid">
+
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box">
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
+                                <li class="breadcrumb-item active">Datatables</li>
+                            </ol>
+                        </div>
+                        <h4 class="page-title">All Customer</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- end page title -->
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+
+                            <table id="basic-datatable" class="table dt-responsive nowrap w-100">
+                                <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Shop Name</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+
+
+                                <tbody>
+                                @foreach($allCustomer as $customer)
+                                <tr>
+                                    <td>{{ $customer->id }}</td>
+                                    <td><img
+                                        src="{{ !empty($customer->image) ?
+                                                asset($customer->image) :
+                                                url('upload/no_image.jpg') }}"
+                                        width="50px"
+                                        height="40px"
+                                        />
+                                    </td>
+                                    <td>{{ $customer->name }}</td>
+                                    <td>{{ $customer->email }}</td>
+                                    <td>{{ $customer->phone }}</td>
+                                    <td>{{ $customer->shopname }}</td>
+                                    <th>
+                                        <a href="{{ route('customer.edit',$customer->id) }}" class="btn btn-primary">edit</a>
+                                        <a href="{{ route('customer.destroy',$customer->id) }}" class="btn btn-danger" id="delete">delete</a>
+                                    </th>
+                                </tr>
+                                @endforeach
+
+                                </tbody>
+                            </table>
+
+                        </div> <!-- end card body-->
+                    </div> <!-- end card -->
+                </div><!-- end col-->
+            </div>
+            <!-- end row-->
+        </div> <!-- container -->
+
+    </div> <!-- content -->
+
+@endsection
